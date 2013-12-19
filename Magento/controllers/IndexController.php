67de20c5e947b536e1634ba51efb4864830ad87d
pankaj <pankajs@ocodewire.com>
2013-12-19 16:58:38 +0530

    Sample code added to Magento

---
+++ Magento/controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Ocodewire_Dailydeal Product widget
+ *
+ * @category    Catalog
+ * @package     Ocodewire_Dailydeal
+ * @author      Ocodewire <magento@browsewire.net>
+ */
+
+class Ocodewire_Dailydeal_IndexController extends Mage_Core_Controller_Front_Action
+{
+	//indexAction is the default Action for any controller
+	function indexAction() {
+		
+	}
+}
