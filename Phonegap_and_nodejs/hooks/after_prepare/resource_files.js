15d8a505839b69e2eb8268651b593d01fb0bcb4f
aniljailta <aniljailta@gmail.com>
2015-02-04 18:24:31 +0530

    Updated resource edit file

--- Phonegap_and_nodejs/hooks/after_prepare/resource_files.js
+++ Phonegap_and_nodejs/hooks/after_prepare/resource_files.js
@@ -6,25 +6,25 @@
 
 // configure all the files to copy.  Key of object is the source file, value is the destination location.  It's fine to put all platforms' icons and splash screen files here, even if we don't build for all platforms on each developer's box.
 var filestocopy = [
-  {'_resources/icons/icon-40.png'       : 'platforms/ios/Maths Tutor/Resources/icons/icon-40.png'},
-  {'_resources/icons/icon-40@2x.png'    : 'platforms/ios/Maths Tutor/Resources/icons/icon-40@2x.png'},
-  {'_resources/icons/icon-50.png'       : 'platforms/ios/Maths Tutor/Resources/icons/icon-50.png'},
-  {'_resources/icons/icon-50@2x.png'    : 'platforms/ios/Maths Tutor/Resources/icons/icon-50@2x.png'},
-  {'_resources/icons/icon-60.png'       : 'platforms/ios/Maths Tutor/Resources/icons/icon-60.png'},
-  {'_resources/icons/icon-60@2x.png'    : 'platforms/ios/Maths Tutor/Resources/icons/icon-60@2x.png'},
-  {'_resources/icons/icon-72.png'       : 'platforms/ios/Maths Tutor/Resources/icons/icon-72.png'},
-  {'_resources/icons/icon-72@2x.png'    : 'platforms/ios/Maths Tutor/Resources/icons/icon-72@2x.png'},
-  {'_resources/icons/icon-76.png'       : 'platforms/ios/Maths Tutor/Resources/icons/icon-76.png'},
-  {'_resources/icons/icon-76@2x.png'    : 'platforms/ios/Maths Tutor/Resources/icons/icon-76@2x.png'},
-  {'_resources/icons/icon-small.png'    : 'platforms/ios/Maths Tutor/Resources/icons/icon-small.png'},
-  {'_resources/icons/icon-small@2x.png' : 'platforms/ios/Maths Tutor/Resources/icons/icon-small@2x.png'},
-  {'_resources/icons/icon.png'          : 'platforms/ios/Maths Tutor/Resources/icons/icon.png'},
-  {'_resources/icons/icon@2x.png'       : 'platforms/ios/Maths Tutor/Resources/icons/icon@2x.png'},
+  {'_resources/icons/icon-40.png'       : 'platforms/ios/HCC/Resources/icons/icon-40.png'},
+  {'_resources/icons/icon-40@2x.png'    : 'platforms/ios/HCC/Resources/icons/icon-40@2x.png'},
+  {'_resources/icons/icon-50.png'       : 'platforms/ios/HCC/Resources/icons/icon-50.png'},
+  {'_resources/icons/icon-50@2x.png'    : 'platforms/ios/HCC/Resources/icons/icon-50@2x.png'},
+  {'_resources/icons/icon-60.png'       : 'platforms/ios/HCC/Resources/icons/icon-60.png'},
+  {'_resources/icons/icon-60@2x.png'    : 'platforms/ios/HCC/Resources/icons/icon-60@2x.png'},
+  {'_resources/icons/icon-72.png'       : 'platforms/ios/HCC/Resources/icons/icon-72.png'},
+  {'_resources/icons/icon-72@2x.png'    : 'platforms/ios/HCC/Resources/icons/icon-72@2x.png'},
+  {'_resources/icons/icon-76.png'       : 'platforms/ios/HCC/Resources/icons/icon-76.png'},
+  {'_resources/icons/icon-76@2x.png'    : 'platforms/ios/HCC/Resources/icons/icon-76@2x.png'},
+  {'_resources/icons/icon-small.png'    : 'platforms/ios/HCC/Resources/icons/icon-small.png'},
+  {'_resources/icons/icon-small@2x.png' : 'platforms/ios/HCC/Resources/icons/icon-small@2x.png'},
+  {'_resources/icons/icon.png'          : 'platforms/ios/HCC/Resources/icons/icon.png'},
+  {'_resources/icons/icon@2x.png'       : 'platforms/ios/HCC/Resources/icons/icon@2x.png'},
 
-  {'_resources/splash/Default-Landscape@2x~ipad.png'  : 'platforms/ios/Maths Tutor/Resources/splash/Default-Landscape@2x~ipad.png'},
-  {'_resources/splash/Default-Landscape~ipad.png'     : 'platforms/ios/Maths Tutor/Resources/splash/Default-Landscape~ipad.png'},
-  {'_resources/splash/Default-Portrait@2x~ipad.png'   : 'platforms/ios/Maths Tutor/Resources/splash/Default-Portrait@2x~ipad.png'},
-  {'_resources/splash/Default-Portrait~ipad.png'      : 'platforms/ios/Maths Tutor/Resources/splash/Default-Portrait~ipad.png'}
+  {'_resources/splash/Default-Landscape@2x~ipad.png'  : 'platforms/ios/HCC/Resources/splash/Default-Landscape@2x~ipad.png'},
+  {'_resources/splash/Default-Landscape~ipad.png'     : 'platforms/ios/HCC/Resources/splash/Default-Landscape~ipad.png'},
+  {'_resources/splash/Default-Portrait@2x~ipad.png'   : 'platforms/ios/HCC/Resources/splash/Default-Portrait@2x~ipad.png'},
+  {'_resources/splash/Default-Portrait~ipad.png'      : 'platforms/ios/HCC/Resources/splash/Default-Portrait~ipad.png'}
 ];
 
 var fs = require('fs');
